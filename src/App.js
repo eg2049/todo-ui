e7130e84c99fa8d07c6a7c94151320b256e89c91
evgenius2049 <evgenius2049@tutanota.com>
2022-08-21 16:00:09 +0300

    добавление компонентов Navbar.jsx и TodoList.jsx & включение их в App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Navbar from "./components/UI/navbar/Navbar";
+import TodoList from "./components/TodoList";
 import "./styles/App.css";
 
 function App() {
     return (
-        <div>
-            Todo App
+        <div className="container">
+            <Navbar />
+            <TodoList />
         </div>
     );
 }
