c0ba4e9f73e279374a6626ab81c221d3abe7c03a
evgenius2049 <evgenius2049@tutanota.com>
2022-08-21 16:18:50 +0300

    добавление компонента-страницы Error.jsx

---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <h3>
+            Вы ввели несуществующий URL!
+        </h3>
+    );
+};
+
+export default Error;
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
+import Error from "../pages/Error";
 import Todos from "../pages/Todos";
 import { todoUIEndpoints } from "../config";
 
 export const routes = [
-    { path: todoUIEndpoints.getTodoList, element: <Todos />, exact: true }
+    { path: todoUIEndpoints.getTodoList, element: <Todos />, exact: true },
+    { path: todoUIEndpoints.notFound, element: <Error />, exact: false }
 ];
