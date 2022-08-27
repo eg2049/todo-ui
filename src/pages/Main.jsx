06af773b6928323632a4b36377f1ac171ddd4cc3
evgenius2049 <evgenius2049@tutanota.com>
2022-08-27 11:31:37 +0300

    проверка аутентификации по localStorage при отображении содержимого Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,24 @@
-import React from "react"
+import React from "react";
 import TodoForm from "../components/TodoForm";
 import TodoList from "../components/TodoList";
+import { authTokenHeadersGet } from "../handlers/authHandlers";
 
 const Main = () => {
+    const isAuth = authTokenHeadersGet();
+
     return (
         <div>
-            <TodoForm />
-            <TodoList />
+            {isAuth.Authorization
+                ?
+                <div>
+                    <TodoForm />
+                    <TodoList />
+                </div>
+                :
+                <h3 className="position-absolute top-50 start-50 translate-middle">
+                    Please log in to manage your todo list
+                </h3>
+            }
         </div>
     );
 };
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound = () => {
     return (
         <h3 className="position-absolute top-50 start-50 translate-middle">
-            Вы ввели несуществующий URL!
+            Page not found =(
         </h3>
     );
 };
